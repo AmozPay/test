659ec7d0ddcdf7f14cb6157b431ce2d4a69684bb
AmozPay <amoz.pay@epitech.eu>
2023-02-25 17:47:54 +0100

    create sample server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+const express = require("express")
+
+app = express()
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(8080, '0.0.0.0', () => console.log("listening on 8080"))
