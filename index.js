45e24204b6ac6eafce9da646ecc2b4ba822a6614
AmozPay <amoz.pay@epitech.eu>
2023-02-26 00:01:05 +0100

    change port

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ app = express()
 
 app.get("/", (req, res) => res.send("Hello World"));
 
-app.listen(8080, '0.0.0.0', () => console.log("listening on 8080"))
+app.listen(80, '0.0.0.0', () => console.log("listening on 80"))
