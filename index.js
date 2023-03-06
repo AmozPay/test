9ade50c233504b57bc1c2cb02d26600148edec11
AmozPay <amoz.pay@epitech.eu>
2023-03-06 18:54:40 +0100

    fourme

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express")
 
 app = express()
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello Fourmey"));
 
 app.listen(80, '0.0.0.0', () => console.log("listening on 80"))
